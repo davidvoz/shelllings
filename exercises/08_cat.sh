4fed2522dadc3b9a4198d57163ddaaa942fe3930
user <vozny.david@gmail.com>
2025-07-03 23:54:21 -0700

    exercise 8 cat head and tail added

---
+++ exercises/08_cat.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# We briefly mentioned the cat command earlier. It prints out all the
+# data of a file into the terminal. And like the animal, 2 more commands
+# are used. Head to print out the first lines of a file and tail to
+# print out the last lines of a file.
+#
+# $ cat file		# prints out the entiretly of a file
+# $ head file		# prints out the first 10 lines of a file
+# $ tail file		# prints out the last 10 lines of a file
+# $ head -n 1		# prints out only the first line of a file
+# $ tail -n 18		# prints out only the last 18 lines of a file
+# 
+# Fix the command below to do a word count on the last 50 lines of the
+# LICENSE file in this repository. Don't change the new file name.
+
+head 50 LICENSE > 08_cat # the '>' sign is discussed later. For now
+						 # just know that it is outputting the command
+						 # into a file called 08_cat
+wc -l 08_cat
---
+++ tests/08_cat.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+RED="\033[31m"
+GREEN="\033[32m"
+RESET="\033[0m"
+
+cleanup() {
+	[ -f "08_cat" ] && rm 08_cat
+}
+trap cleanup EXIT
+
+output=$(sh exercises/08_cat.sh)
+answer=$(tail -n 50 LICENSE | wc -w)
+
+if [ $(wc -w < 08_cat) -eq $answer ]; then
+	echo "${GREEN}Passed${RESET}"
+else
+	echo "${RED}Failed${RESET}"
+fi
