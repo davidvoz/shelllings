abd6754433c3b47e8268e17b9f46ab7304fd38cb
user <vozny.david@gmail.com>
2025-07-03 18:26:52 -0700

    added stuff

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # shelllings
-a practical way to learn shell
+
+## about
+A practical way of learning shell through exercises.
+This has a focus on a linux environment.
+This is also mainly a way for me to learn shell as well so bugs and unoptimized code should be expected.
+
+## installing and running
+
+```
+git clone https://github.com/davidvoz/shelllings.git
+cd shelllings
+sh run_me.sh
+```
---
+++ run_me.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+BLUE="\033[34m"
+RED="\033[31m"
+GREEN="\033[32m"
+RESET="\033[0m"
+
+pause() {
+	printf "%s" "$1"
+	read user_input
+	echo ""
+}
+
+pause "Welcome to Shelllings [press ENTER]"
+
+echo "This program is meant to help you understand most posix shells,"
+echo "like bash or zsh. You already ran the command 'sh run_shell.sh'"
+echo "but the command 'sh' doesn't really exist on most systems."
+echo "Run 'ls -l /bin/sh' to see if there is a link to another program."
+pause "Let's test it out [press ENTER for ls -l /bin/sh]"
+
+echo "${BLUE}$(ls -l /bin/sh)${RESET}"
+
+USER_SHELL=$(ls -l /bin/sh | awk '{print $NF}')
+
+echo ""
+if [ "$USER_SHELL" = "sh" ]; then 
+	echo "Looks like you are actually using sh, not everyone has it"
+	printf "like this. Many shells are changed. "
+else
+	echo "From the command above, we can see that you are actually"
+	printf "running ${BLUE}$USER_SHELL${RESET} under the hood. "
+fi
+
+echo "Bash is switched to a "
+echo "POSIX-compatible mode, dash also only implements POSIX features."
+echo "There are many different shells but we will be sticking with"
+echo "POSIX compliant programs and commands. Meaning just about"
+echo "every shell you use will be able to understand and run these"
+pause "commands. [press ENTER]"
+
+echo "To use the program shelllings, let's run the command"
+echo "'sh shelllings.sh test' to see if it's able to be run"
+pause "[sh shelllings.sh test]"
+
+sh shelllings.sh test && echo "${GREEN}Passed${RESET}"|| {
+	echo "${RED}ERROR failed${RESET}"
+	exit 1
+}
+
+echo ""
+echo "No error message meant that it has not failed. Lets continue on"
+pause "and see what exercises there are right now [ls exercises]"
+
+all_ex=$(ls exercises)
+echo "${BLUE}$all_ex${RESET}"
+
+echo ""
+pause "[press ENTER]"
+
+amount=$(ls exercises | wc -l)
+
+echo "Looks like there are $amount exercises. You should know that"
+echo "you can skip through the ones you already know or come back to"
+echo "ones you already did. You can run 'sh shelllings.sh n' where n"
+echo "is the numbered exercise. You don't need to type the 0's in"
+echo "front. Let's get started with the first exercise"
+pause "[sh shelllings.sh 1]"
+
+sh shelllings.sh 1 || {
+	echo "Looks like it failed. Go on over to the exercises/ folder and fix it"
+}
