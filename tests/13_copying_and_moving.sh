6374b65c7883d7287094a0d759a0509c0bfce2b3
user <vozny.david@gmail.com>
2025-07-05 12:36:54 -0700

    exercise 13 cp and mv added

---
+++ tests/13_copying_and_moving.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+RED="\033[31m"
+GREEN="\033[32m"
+RESET="\033[0m"
+
+failed() {
+	[ -f "11_file" ] && rm 11_file
+	[ -f "11_user_input" ] && rm 11_user_input
+	exit 1
+}
+trap failed EXIT
+
+sh exercises/13_copying_and_moving.sh
+
+[ -f "11_file" ] && {
+	printf "${RED}Failed${RESET}\n"
+	failed
+}
+
+[ -s "11_user_input" ] && printf "${GREEN}Passed${RESET}\n"
