43dc486493953856dcea3cd4937f54d52d97be3b
user <vozny.david@gmail.com>
2025-07-06 14:44:47 -0700

    exercise 18 practical quiz note script added

---
+++ exercises/18_quiz.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Quiz 1 Notes program
+#
+# This program is meant to store notes but has some issues
+
+NOTES_FILE=notes.txt
+
+# a bug always has '-p', when selected, added to notes.txt
+if [ "$*" = "-p"]; then
+	cat notes.txt
+fi
+
+
+if [ "$#" -ge 1] then
+	echo "[$(date)]" >> "$NOTES_FILE" # this line is without fault
+	echo "" >> "$NOTES_FILE"
+else
+	echo "Nothing was added"
+fi
+
+# When you are done, change the NOTES_FILE location and consider putting
+# this into your .local/bin with this part added (yes, you have to find
+# a way to have this inserted into the code above).
+#
+# $ nvim +startinsert /tmp/note_temp
+# $ if [ -s /tmp/note_temp ]; then
+# $		echo "[$(date)]" >> "$NOTES_FILE"
+# $		cat /tmp/note_temp >> "$NOTES_FILE"
+# $		echo "" >> "$NOTES_FILE"
+# $		rm /tmp/note_temp
+# $	fi
+#
+# Don't forget to have that directory path exported as a path in your
+# shell config. Usually, like this
+#
+# export PATH="$HOME/.local/bin:$PATH"
---
+++ tests/18_quiz.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -eu
+RED="\033[31m"
+GREEN="\033[32m"
+RESET="\033[0m"
+
+failed() {
+	[ -f "notes.txt" ] && rm notes.txt
+	printf "${RED}Failed${RESET}\n"
+	exit 1
+}
+
+cleanup() {
+	[ -f "notes.txt" ] && rm notes.txt
+}
+trap cleanup EXIT
+
+sh exercises/18_quiz.sh get oreos for protein shake > /dev/null || failed
+sh exercises/18_quiz.sh -p this line should appear > /dev/null || failed
+sh exercises/18_quiz.sh -p > /dev/null || failed
+sh exercises/18_quiz.sh > /dev/null || failed
+
+[ ! $(grep "get oreos" notes.txt | wc -l) -eq 1 ] && failed
+[ ! $(grep "\-p" notes.txt | wc -l) -eq 1 ] && failed
+
+printf "${GREEN}Passed${RESET}\n"
+
