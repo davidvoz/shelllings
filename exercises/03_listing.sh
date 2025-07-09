19c8f611fba9382a4a0f7d45554d48f1bb19da82
user <vozny.david@gmail.com>
2025-07-08 21:41:37 -0700

    testing spaces vs tabs

--- exercises/03_listing.sh
+++ exercises/03_listing.sh
@@ -4,15 +4,15 @@
 # 'ls' and seeing what is there. Some shell configs will have the 
 # folders bold and different colored to help with navigation.
 # 
-# $ ls		# lists contents
-# $ ls -a	# lists hidden contents as well as unhidden
-# $ ls -A	# same as -a but without . and ..
+# $ ls      # lists contents
+# $ ls -a   # lists hidden contents as well as unhidden
+# $ ls -A   # same as -a but without . and ..
 #             Also, not entirely POSIX compliant but many modern shells
 #             run it fine
-# $ ls -l	# shows linkages in files, like how we did with 
+# $ ls -l   # shows linkages in files, like how we did with 
 #             ls -l /bin/sh
-# $ ls -R	# recursively goes into folders listing contents out
-# $ ls -AR	# separate arguments can be combined to execute both
+# $ ls -R   # recursively goes into folders listing contents out
+# $ ls -AR  # separate arguments can be combined to execute both
 #
 # For your exercise, a random file will be created and hidden. Find
 # the file and delete it.
