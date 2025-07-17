e6527c46837309da544e878a284832a50c76b53d
user <vozny.david@gmail.com>
2025-07-16 19:48:17 -0700

    added an option where you can see which challenges you have completed. This will not work with interactive challenges that require user input

--- shelllings.sh
+++ shelllings.sh
@@ -3,6 +3,27 @@
 EX_DIR="exercises"
 TEST_DIR="tests"
 
+list_prev() {
+	echo "Listing all challenges"
+
+	for file in "$TEST_DIR"/*; do
+		name=$(basename "$file")
+
+		case "$name" in
+			04_navigation.sh|11_userinput.sh|13_copying_and_moving.sh)
+				echo "$name: skipped"
+				continue
+				;;
+		esac
+
+		sh "$file" > /dev/null 2>&1 || echo "$name: failed"
+		sh "$file" > /dev/null 2>&1 && echo "$name"
+	done
+
+	rm -rf blueberry 11_file .youfoundme
+}
+
+
 run_ex() {
 	num="$1"
 	ex_file=$(ls "$EX_DIR"/*.sh | sort | sed -n "${num}p")
@@ -27,15 +48,23 @@ run_ex() {
 
 usage() {
 	echo "Usage"
-	echo "	sh shelllings INT	run the INTth exercise"
-	echo "	sh shelllings test	test the program if runnable"
+	echo "	sh shelllings.sh INT	run the INTth exercise"
+	echo "	sh shelllings.sh list	list the exercises you have completed"
+	echo "	sh shelllings.sh test	test the program if runnable"
 	exit 0
 }
 
-if expr "$1" : '[0-9][0-9]*' > /dev/null; then
-	run_ex "$1"
-elif [ "$1" = "test" ]; then
-	echo "shelllings.sh is working (for now)"
-else 
-	usage
-fi
+case "$1" in
+	test)
+		echo "shelllings.sh is working (for now)"
+		;;
+	list)
+		list_prev
+		;;
+	''|*[!0-9]*)
+		usage
+		;;
+	*)
+		run_ex "$1"
+		;;
+esac
